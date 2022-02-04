411a6c0a15154a26f17b6d56471722559b975357
wilmerojeda13 <ojedawilmerantonio@gmail.com>
2022-02-03 21:50:28 -0500

    add a response to display current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,6 +24,19 @@ function initPage() {
             .then(function (response) {
 
                 todayweatherEl.classList.remove("d-none");
+                // Create a response to display current weather
+                const currentDate = new Date(response.data.dt * 1000);
+                const day = currentDate.getDate();
+                const month = currentDate.getMonth() + 1;
+                const year = currentDate.getFullYear();
+                nameEl.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year + ") ";
+                let weatherPic = response.data.weather[0].icon;
+                currentPicEl.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherPic + "@2x.png");
+                currentPicEl.setAttribute("alt", response.data.weather[0].description);
+                currentTempEl.innerHTML = "Temperature: " + k2f(response.data.main.temp) + " &#176F";
+                currentHumidityEl.innerHTML = "Humidity: " + response.data.main.humidity + "%";
+                currentWindEl.innerHTML = "Wind Speed: " + response.data.wind.speed + " MPH";
+                
 
 
 
