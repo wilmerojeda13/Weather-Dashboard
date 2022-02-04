0c1f8f236277a4c2c2db2ad74c9ed816426b587a
wilmerojeda13 <ojedawilmerantonio@gmail.com>
2022-02-03 21:33:41 -0500

    add function for the id's attributes

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+//Create a principal function for the id's attributes
+
+function initPage() {
+    const cityEl = document.getElementById("enter-city");
+    const searchEl = document.getElementById("search-button");
+    const clearEl = document.getElementById("clear-history");
+    const nameEl = document.getElementById("city-name");
+    const currentPicEl = document.getElementById("current-pic");
+    const currentTempEl = document.getElementById("temperature");
+    const currentHumidityEl = document.getElementById("humidity");
+    const currentWindEl = document.getElementById("wind-speed");
+    const currentUVEl = document.getElementById("UV-index");
+    const historyEl = document.getElementById("history");
+    var fivedayEl = document.getElementById("fiveday-header");
+    var todayweatherEl = document.getElementById("today-weather");
+    let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
